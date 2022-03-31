60b54cba18896fec517181a20a8b1af5dbf19c03
Karina Espinola <k.espinolah@gmail.com>
2022-03-30 18:14:47 -0600

    Add / route to redirect to get oAuth and also /callback

---
+++ main.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv');
+dotenv.config();
+const { TwitterApi } = require('twitter-api-v2');
+const port = 3000
+
+const appOnlyClient = new TwitterApi(process.env.TWITTER_BEARER_TOKEN);
+const rwClient = appOnlyClient.readWrite;
+const codeVerifier = null;
+const state = null;
+
+app.get('/', (req, res) => {
+  const client = new TwitterApi({ clientId: process.env.CLIENT_ID, clientSecret: process.env.CLIENT_SECRET });
+  const { url, codeVerifier, state } = client.generateOAuth2AuthLink(process.env.CALLBACK_URL, { scope: ['tweet.read', 'tweet.write', 'users.read', 'offline.access'] });
+  res.redirect(url);
+});
+
+app.get('/callback', (req, res) => {
+    // Extract state and code from query string
+    const { state, code } = req.query;
+    // Get the saved oauth_token_secret from session
+    const { codeVerifier, state: sessionState } = req.session;
+  
+    if (!codeVerifier || !state || !sessionState || !code) {
+      return res.status(400).send('You denied the app or your session expired!');
+    }
+    if (state !== sessionState) {
+      return res.status(400).send('Stored tokens didnt match!');
+    }
+  
+    // Obtain access token
+    const client = new TwitterApi({ clientId: process.env.CLIENT_ID, clientSecret: process.env.CLIENT_SECRET });
+  
+    client.loginWithOAuth2({ code, codeVerifier, redirectUri: process.env.CALLBACK_URL })
+      .then(async({ client: loggedClient, accessToken, refreshToken, expiresIn }) => {
+        // {loggedClient} is an authenticated client in behalf of some user
+        // Store {accessToken} somewhere, it will be valid until {expiresIn} is hit.
+        // If you want to refresh your token later, store {refreshToken} (it is present if 'offline.access' has been given as scope)
+  
+        // Example request
+        const { data: userObject } = await loggedClient.v2.me();
+        console.log(data);
+      })
+      .catch(() => res.status(403).send('Invalid verifier or access tokens!'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
